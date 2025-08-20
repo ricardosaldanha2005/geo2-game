402e5831b173727501cb2c90af2ed6dfabb75c14
Ricardo <ricardosaldanha2005@gmail.com>
2025-08-20 21:46:42 +0100

    Fix: Remove base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
-  base: '/geo2-game/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
